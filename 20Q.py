6c342d35f320149d6863f79ef3a0f7900886b727
alanmartinezz1 <amartinezz1900@alumno.ipn.mx>
2025-08-29 18:42:46 -0600

    base del código

--- 20Q.py
+++ 20Q.py
@@ -6,49 +6,184 @@ def Juego():
 
     while activo == "si":
         
-        #seres vivos
-            resp = input("¿Es un ser vivo?")
+        #seres vivos ---------------------------
+        resp = input("¿Es un ser vivo? ")
+        if resp == "si":
+
+            #animales ------------------------
+            resp = input("¿Es capaz de moverse por sí mismo? ")
             if resp == "si":
 
-                #animales
-                resp = input("¿es capaz de moverse por sí mismo?")
+                #personas ---------------------------
+                resp = input("¿Tiene una alta capacidad de razonamiento? ")
                 if resp == "si":
 
-                    #personas
-                    resp = input("¿Tiene una alta capacidad de razonamiento?")
+                    resp = input("¿Es una figura histórica o actual? ")
                     if resp == "si":
 
-                        #
+                        #artista
+                        resp = input("¿Es famoso en el arte? ")
+                        if resp == "si":
+
+                            resp = input("")
+                            
+                        else:
+                            #Cientifico
+                            resp = input("¿Es reconocido por sus descubrimientos científicos? ")
+                            if resp == "si":
+
+                                resp = input("")
+
+                            else:
+                                #politico
+                                resp = input("¿Es una figura política reconocida? ")
+                                if resp == "si":
+
+                                    resp = input("")
+                                else:
+                                    #actor
+                                    resp = input("¿Es actor o actriz? ")
+                                    if resp == "si":
+
+                                        resp = input("")
+
+                    else:
                         resp = input("")
 
-                    #aereos
-                    resp = input("¿puede volar?")
-                    if resp == "si":
-                        #
-                        resp = input("")
-                
-                    #acuatico
-                    resp = input("¿habita en el agua?")
-                    if resp == "si":
-                        #
-                        resp = input("")
+                else:
 
-                    #terrestre
-                    resp = input("¿es carnivoro?")
+                    #animales aéreos ----------------------------------
+                    resp = input("¿Puede volar? ")
                     if resp == "si":
-                        resp = input("")
-                
-                #plantas
-                resp = input("")
+
+                        resp = input("¿Es un ave de gran tamaño? ")
+                        if resp == "si":
+
+                            resp = input("¿Tiene un cuello largo? ")
+                            if resp == "si":
+
+                                resp = input("")
+                        else:
+                            #PALOMA
+                            resp = input("¿Suele vivir cerca de humanos? ")
+                            if resp == "si":
+                                
+                                print("Tu animal es un cangrejo")
+
+                            else:
+                                resp = input("¿Tiene colores llamativos? ")
+                                if resp == "si":
+
+                                    resp = input("")
+
+                    else:
+                        #animales acuáticos ---------------------------------------
+                        resp = input("¿habita en el agua?")
+                        if resp == "si":
+
+                            resp = input("¿Es un mamífero marino?")
+                            if resp == "si":
+
+                                resp = input("¿Es muy grande?")
+                                if resp == "si":
+
+                                    resp = input("")
+                            else:
+                                #TORTUGA
+                                resp = input("¿Tiene concha o caparazón?")
+                                if resp == "si":
+
+                                    print("Tu animal es una tortuga marina")
+                                else:
+                                    #CANGREJO
+                                    resp = input("¿Tiene pinzas o tenazas?")
+                                    if resp == "si":
+
+                                        print("Tu animal es un cangrejo")
+                                    else:
+
+                                        #PULPO
+                                        resp = input("¿Tienetentáculos?")
+                                        if resp == "si":
+
+                                            print("Tu animal es un pulpo")
+                                        else:
+
+                                            resp = input("¿Tiene cuerpo alargado y aletas?")
+                                            if resp == "si":
+
+                                                #TIBURON
+                                                resp = input("¿Es un pez depredador?")
+                                                if resp == "si":
+                                                    
+                                                    print("Tu animal es un tiburón")
+                                                
+                                            else:
+                                                #MEDUSA
+                                                print("Tu animal es una medusa")
+
+
+                        else:
+                            #animales terrestres -------------------------------
+                            resp = input("¿Es carnívoro? ")
+                            if resp == "si":
+
+                                #Felino
+                                resp = input("¿Es un felino? ")
+                                if resp == "si":
+
+                                    #LEON
+                                    resp = input("¿Tiene melena? ")
+                                    if resp == "si":
+
+                                        print("Es un león")
+
+                                    else:
+                                        #TIGRE
+                                        print("Es un tigre")
+                                else:
+                                    resp = input("¿Es un canino? ")
+                                    if resp == "si":
+
+                                        resp = input("")
+
+                                    else:
+                                        resp = input("")
+
+                            else:
+
+                                resp = input("¿Es un animal de granja? ")
+                                if resp == "si":
+
+                                    #VACA
+                                    resp = input("¿Da leche? ")
+                                    if resp == "si":
+
+                                        print("Es una vaca")
+                                    
+                                    resp = input("")
+
+
+                                else:
+                                    resp = input("¿Es un roedor? ")
+                                    if resp == "si":
+
+                                        resp = input("")
+                                    
+                                    resp = input("")
+
+            else:
+                #plantas -------------------------------------------------------------
+                resp = input("¿? ")
                 if resp == "si":
-                    
+
                     resp = input("")
+                   
 
 
         #comida
-            if resp == "si":
-                #
-                resp = input("")
+
+            resp = input("")
 
         #instrumentos musicales
 
